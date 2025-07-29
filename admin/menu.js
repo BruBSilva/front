39637e769561df87af9868e05b8e86a92f4a12de
Vitória Silva <silva.lopes.vitoria@gmail.com>
2025-07-29 18:21:12 -0300

    Corrigindo requisições e adicionando login

--- admin/menu.js
+++ admin/menu.js
@@ -1,8 +1,19 @@
+const user = sessionStorage.getItem('user');
+const token = user ? JSON.parse(user).token : null;
+
 document.addEventListener("DOMContentLoaded", function() {
+  if (!sessionStorage.getItem('user')) {
+    window.location.href = '/admin/login.html';
+  }
+
   fetch('/admin/menu.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('menu-container').innerHTML = data;
-    })
-    .catch(error => console.error('Erro ao carregar o menu:', error));
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('menu-container').innerHTML = data;
+    document.getElementById('btnLogout').addEventListener('click', function() {
+      sessionStorage.removeItem('user');
+      window.location.href = '/admin/login.html';
+    });
+  })
+  .catch(error => console.error('Erro ao carregar o menu:', error));
 });
